2642b081239a76561910dbad46033d1661707467
engp8691 <li_yonglin@yahoo.com>
2019-04-25 23:28:20 -0400

    For Demo

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const geocode = require('./utils/geocode');
 const forecast= require('./utils/forecast');
 
-geocode("Boston", (err, data)=>{
-	if(err)  console.log("Err: ", err);
-	if(data){
-		forecast(data, (err, data)=>{
-			if(err)  console.log(err);
-			if(data) console.log(data);
-		});
-	}
-});
+let place = 'Boston';
 
-geocode("Sioux Falls", (err, data)=>{
-	if(err)  console.log("Err: ", err);
+if (process.argv.length === 3){
+	place = process.argv[2];
+}else{
+	console.log('Usage: node app.js "place name"');
+	console.log('It uses default place of Boston');
+}
+
+geocode(place, (err, data)=>{
+	if(err)  console.log('Err: ', err);
 	if(data){
 		forecast(data, (err, data)=>{
 			if(err)  console.log(err);
